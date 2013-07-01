3d4b459a49c0165da10cccddbbcc879a76661033
Andy Holt <andrew.holt@hotmail.co.uk>
2013-07-01 19:18:12 +0100

    setrequired script added

---
+++ JourneyTimes/setrequired.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""
+Add the line data for each station
+"""
+import xml.etree.ElementTree as ET
+
+allstations_tree = ET.parse('AllStations.xml')
+allstations_root = allstations_tree.getroot()
+
+required_tree = ET.parse('London270.xml')
+required_root = required_tree.getroot()
+
+for station in allstations_root:
+    for reqstation in required_root:
+        if station.attrib == reqstation.attrib:
+            station.__getitem__(0).text = "true"
+
+allstations_tree.write('AllStations.xml')
