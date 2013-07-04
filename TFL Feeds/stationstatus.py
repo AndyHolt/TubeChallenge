d81d761517d0cb02311123fa5bcc6d4e195af720
Andy Holt <andrew.holt@hotmail.co.uk>
2013-07-04 15:14:39 +0100

    Print out station names.
    stationstatus script will print station names. To be added is relevant
    data about each station, and only print those station which contain
    relevant information.

--- TFL Feeds/stationstatus.py
+++ TFL Feeds/stationstatus.py
@@ -3,10 +3,19 @@
 Fetch news from TFL and display in terminal
 """
 import urllib.request
+import xml.etree.ElementTree as ET
 
 station_status = urllib.request.urlopen('http://cloud.tfl.gov.uk/TrackerNet/StationStatus')
+
+print(station_status.info())
 
 output_file = open('stationstatus_out.xml',mode='wb')
 
 #print(station_status.read())
 output_file.write(station_status.read())
+
+station_data = ET.parse('stationstatus_out.xml')
+station_data_root = station_data.getroot()
+
+for station_status_id in station_data_root:
+    print(station_status_id[0].get('Name'))
