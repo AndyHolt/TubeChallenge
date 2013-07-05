e810434991e08a531070baf1ecd9e795c43b8184
Andy Holt <ah635@cam.ac.uk>
2013-07-05 19:19:37 +0100

    Both journey planner test scripts working.
    bug fix in python to write xml file correctly.
    Removed extra debugging output and made output messages clearer.

--- TFL Feeds/journeyplannertest.py
+++ TFL Feeds/journeyplannertest.py
@@ -9,6 +9,6 @@ import urllib
 plan = urllib.urlopen(
     'http://jpapi.tfl.gov.uk/api/XML_TRIP_REQUEST2?language=en&sessionID=0&place_origin=London&type_origin=stop&name_origin=Alexander%20Road&place_destination=London&type_destination=stop&name_destination=Price%20Of%20Wales%20Gate&itdDate=20130706&itdTime=0800')
 f = open('journeyplannertest.xml', 'r+')
-s = str(f.read())
+s = str(plan.read())
 f.write(s)
 f.close()
