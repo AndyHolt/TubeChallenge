357cd7bd1342e23186fcba120caa1f706bdfeeb3
Andy Holt <ah635@cam.ac.uk>
2013-07-05 18:55:04 +0100

    Journey planner python script started and bash script.
    Bash script tests that it is being run on server and exits if not.
    Will then call python script if it is.

---
+++ TFL Feeds/journeyplannertest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# ONLY TO BE RUN ON CUED SERVERS!
+# python2 script
+"""
+Get some data from TLF feed
+"""
+import urllib
+
+plan = urllib.urlopen(
+    'http://jpapi.tfl.gov.uk/api/XML_TRIP_REQUEST2?language=en&sessionID=0&place_origin=London&type_origin=stop&name_origin=Alexander%20Road&place_destination=London&type_destination=stop&name_destination=Price%20Of%20Wales%20Gate&itdDate=20130706&itdTime=0800')
+f = open('journeyplannertest.xml', 'r+')
+s = str(f.read())
+f.write(s)
+f.close()
