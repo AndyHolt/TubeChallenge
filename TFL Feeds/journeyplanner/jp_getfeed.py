ce12772c0c352cd03fdb0f67d38b4013f53310c9
Andy Holt <ah635@cam.ac.uk>
2013-07-10 15:47:31 +0100

    Journey planner: split url into multiple parts.
    Makes it easier to change stations, date or time of the searched url.
    Will eventually become a function where these are passed.

--- TFL Feeds/journeyplanner/jp_getfeed.py
+++ TFL Feeds/journeyplanner/jp_getfeed.py
@@ -7,8 +7,22 @@ Get some data from TLF feed
 import urllib
 import xml.etree.ElementTree as ET
 
-plan = urllib.urlopen(
-    'http://jpapi.tfl.gov.uk/api/XML_TRIP_REQUEST2?language=en&sessionID=0&place_origin=London&type_origin=stop&name_origin=Amersham%20Undergroud%20Station&place_destination=London&type_destination=stop&name_destination=Chalfont%20%26%20Latimer%20Underground%20Station&itdDate=20130711&itdTime=0800')
+station_start = "Amersham Underground Station"
+station_end = "Chalfont & Latimer Underground Station"
+journey_date = "20130711"
+journey_time = "0800"
+
+url_base = "http://jpapi.tfl.gov.uk/api/XML_TRIP_REQUEST2?language=en&sessionID=0"
+url_jstart = "&place_origin=London&type_origin=stop&name_origin="
+url_jend ="&place_destination=London&type_destination=stop&name_destination="
+url_date = "&itdDate="
+url_time = "&itdTime="
+
+url = url_base + url_jstart + urllib.quote(station_start) + url_jend + \
+    urllib.quote(station_end) + url_date + journey_date + url_time + \
+    journey_time
+
+plan = urllib.urlopen(url)
 f = open('journeyplannertest.xml', 'r+')
 s = str(plan.read())
 f.write(s)
