c6c90bb87a5a7f6130caf1aebd0d9939fb1ff611
Andy Holt <andrew.holt@hotmail.co.uk>
2013-07-16 18:28:38 +0100

    Add script to run full network.

---
+++ TFL Feeds/journeyplanner/graph_run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+Build graph of 270 stations. Only produce an edge if there is a direct
+connection between the stations.
+"""
+# Author: Andy Holt
+# Date: Tue 16 Jul 2013 18:27
+# Usage: Only to be run on CUED servers.
+
+from Station import Station, StationList
+from Journey import Journey
+from GraphBuilder import GraphBuilder
+
+full_station_list = StationList()
+full_station_list.load_270()
+
+my_graph_builder = GraphBuilder(full_station_list)
+my_graph_builder.build_no_change_graph()
+my_graph_builder.export_to_gexf()
+
+#print my_graph_builder.journey_time_matrix
+
